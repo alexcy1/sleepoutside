db3c96cd87c75200925085e26992aea68dfa8c23
alexcy1 <cyr23001@byui.edu>
2025-01-09 11:08:12 +0100

    Test commit

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,23 +1,61 @@
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
+// function convertToJson(res) {
+//   if (res.ok) {
+//     return res.json();
+//   } else {
+//     throw new Error("Bad Response");
+//   }
+// }
+
+// export default class ProductData {
+//   constructor(category) {
+//     this.category = category;
+//     this.path = `../json/${this.category}.json`;
+//   }
+//   getData() {
+//     return fetch(this.path)
+//       .then(convertToJson)
+//       .then((data) => data);
+//   }
+//   async findProductById(id) {
+//     const products = await this.getData();
+//     return products.find((item) => item.Id === id);
+//   }
+// }
+
+
+
+
+
+
+const baseURL = '../json';
 
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
+    this.path = `${baseURL}/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+
+  async getData() {
+    try {
+      const response = await fetch(this.path);
+      if (!response.ok) {
+        throw new Error('Bad Response');
+      }
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.error('Error fetching data:', err);
+      throw err;
+    }
   }
+
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    try {
+      const products = await this.getData();
+      return products.find((item) => item.Id === id);
+    } catch (err) {
+      console.error('Error finding product:', err);
+      throw err;
+    }
   }
 }
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -33,6 +33,65 @@
 
 // =================================================================
 
+// // wrapper for querySelector...returns matching element
+// export function qs(selector, parent = document) {
+//   return parent.querySelector(selector);
+// }
+
+// // retrieve data from localstorage
+// export function getLocalStorage(key) {
+//   try {
+//     const item = localStorage.getItem(key);
+//     return item ? JSON.parse(item) : [];
+//   } catch (e) {
+//     console.error("Error getting data from localStorage:", e);
+//     return [];
+//   }
+// }
+
+// // save data to local storage
+// export function setLocalStorage(key, data) {
+//   try {
+//     let existingData = getLocalStorage(key);
+
+//     // If existingData is not an array, initialize it
+//     if (!Array.isArray(existingData)) {
+//       existingData = [];
+//     }
+
+//     // Add new data to existing array
+//     existingData.push(data);
+
+//     // Save back to localStorage
+//     localStorage.setItem(key, JSON.stringify(existingData));
+//   } catch (e) {
+//     console.error("Error saving data to localStorage:", e);
+//   }
+// }
+
+// // set a listener for both touchend and click
+// export function setClick(selector, callback) {
+//   const element = qs(selector);
+//   if (element) {
+//     element.addEventListener("touchend", (event) => {
+//       event.preventDefault();
+//       callback();
+//     });
+//     element.addEventListener("click", callback);
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 // wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
@@ -80,3 +139,4 @@ export function setClick(selector, callback) {
     element.addEventListener("click", callback);
   }
 }
+
